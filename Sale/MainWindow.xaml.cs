2c4badd714f862affc27562584f2977f38892e97
Vladislav Skalozub <vladskalozub97@gmail.com>
2023-01-19 19:39:04 +0200

    update

--- Sale/MainWindow.xaml.cs
+++ Sale/MainWindow.xaml.cs
@@ -50,8 +50,13 @@ namespace Sale
             ShowDepartmentsCount();
             ShowManagersCount();
             ShowProductsCount();
+            ShowSalesCount();
+            ShowDailyStatistic();
         }
 
+
+
+        #region Show Monitor
         /// <summary>
         /// Выводит в таблицу монитор количество отделов (департамментов)
         /// </summary>
@@ -65,7 +70,7 @@ namespace Sale
 
         private void ShowManagersCount()
         {
-            String sql = "SELECT COUNT(*) FROM Departments\r\n";
+            String sql = "SELECT COUNT(*) FROM Managers\r\n";
             using var cmd = new SqlCommand(sql, _connection);
             MonitorManagers.Content = Convert.ToString(cmd.ExecuteScalar());
 
@@ -73,10 +78,57 @@ namespace Sale
 
         private void ShowProductsCount()
         {
-            String sql = "SELECT COUNT(*) FROM Departments\r\n";
+            String sql = "SELECT COUNT(*) FROM Products\r\n";
             using var cmd = new SqlCommand(sql, _connection);
             MonitorProducts.Content = Convert.ToString(cmd.ExecuteScalar());
 
         }
+
+
+        private void ShowSalesCount()
+        {
+            String sql = "SELECT COUNT(*) FROM Sales\r\n";
+            using var cmd = new SqlCommand(sql, _connection);
+            MonitorSales.Content = Convert.ToString(cmd.ExecuteScalar());
+
+        }
+
+        #endregion
+
+
+        /// <summary>
+        /// Заполняет блок "Статистика за день"
+        /// </summary>
+        private void ShowDailyStatistic()
+        {
+            SqlCommand cmd = new SqlCommand()
+            {
+                Connection = _connection
+
+            };
+            // В БД информация за 2022 год, поэтому формируя дату с текущим днем и месяцем, но за 2022
+            String date = $"2022-{DateTime.Now.Month}-{DateTime.Now.Day}";
+
+            // Всего продаж (чеков)
+            cmd.CommandText = $"SELECT COUNT(*) FROM Sales S WHERE CAST( S.Moment AS DATE ) = '{date}'\r\n";
+            StatTotalSales.Content = Convert.ToString(cmd.ExecuteScalar());
+
+            // Всего продаж (товарров, штук)
+            cmd.CommandText = $"SELECT SUM(S.Cnt) FROM Sales S WHERE CAST( S.Moment AS DATE ) = '{date}'\r\n";
+            StatTotalProducts.Content = Convert.ToString(cmd.ExecuteScalar());
+            cmd.Dispose();
+
+            // Всего продаж (грн, деньги)
+            cmd.CommandText = cmd.CommandText = $"SELECT ROUND( SUM( S.Cnt * P.Price ), 2 ) FROM Sales S JOIN Products P ON S.Id_product = P.Id WHERE CAST( S.Moment AS DATE ) = '{date}'";
+            StatTotalMoney.Content = Convert.ToString(cmd.ExecuteScalar());
+            cmd.Dispose();
+
+        }
     }
 }
+
+/* Дополнить блок "Статистика за день" данными следующих категорий:
+ * Самый эффективный менеджер [Фамилия, Имя](по деньгам)
+ * Самый эффективный отдел [Название]( по кол-ву проданных товаров)
+ * Самый популярный товар [Название] (по кол-ву чеков)
+*/
